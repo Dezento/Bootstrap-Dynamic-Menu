0a38059d8cd5df184ca702cdf7e9051543b78070
Dezento <milovanpanic92@gmail.com>
2017-06-16 18:46:19 +0200

    Add files via upload
    Example structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require "header.php"; ?>
+
+
+
+<div class="container">
+  <h3>Basic Dynamic Menu Example</h3>
+  <p>This is Index page</p>
+</div>
+
+<?php require "footer.php"; ?>
---
+++ page1.php
@@ -0,0 +1,10 @@
+<?php require "header.php"; ?>
+
+
+
+<div class="container">
+  <h3>Basic Dynamic Menu Example</h3>
+  <p>This is Page 1 </p>
+</div>
+
+<?php require "footer.php"; ?>
