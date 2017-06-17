253fc2694429a3f056e977c61843972f1713cd24
Nenad Novakovic <n.dvLden@gmail.com>
2017-06-17 21:25:33 +0200

    complete rewrite of a plugin, but demo refactoring as well...

---
+++ demo/page-1.php
@@ -0,0 +1,8 @@
+<?php include 'partials/top.php'; ?>
+
+<div class="container">
+    <h3>Basic Dynamic Menu Example</h3>
+    <p>This is Page 1</p>
+</div>
+
+<?php include 'partials/bottom.php'; ?>
---
+++ demo/partials/bottom.php
@@ -0,0 +1,9 @@
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../dynamic-menu.js"></script>
+
+    <script>
+        $('#main-navbar').dynamicMenu();
+    </script>
+</body>
+</html>
